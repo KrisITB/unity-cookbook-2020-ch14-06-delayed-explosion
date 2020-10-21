6b5c92004bdccd879470e71c3ec16488a0e7abd2
Kris <b00064110@student.itb.ie>
2020-10-21 21:10:35 +0100

    Update BombBehaviour.cs
    exploding(i.e.: disappearing) dynamic objects

--- Assets/_Scripts/BombBehaviour.cs
+++ Assets/_Scripts/BombBehaviour.cs
@@ -21,10 +21,23 @@ public class BombBehaviour : MonoBehaviour
         Destroy(explosion, 1);
         
         // --- do any collision logic for bomb here -----
-        //  Collider[] hitColliders = Physics.OverlapSphere(center, radius);
-
-        
-        // destroy this bomb GameObject
         Destroy(gameObject);
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 2f);
+        foreach(Collider coll in hitColliders)
+        {
+            //if(coll.gameObject.layer== 8) // better approach IMHO but it requiers a custom layer added and assigned to objects that are destructable
+            if(coll.transform.parent != null)//requiered otherwise foreach loop seems to break when object has no parent
+            {
+                if (coll.transform.parent.transform.name == "GeometryDynamic") //all Dynamic objects in this scene i.e.: Pickup and BoxSmall
+                {
+                    Destroy(coll.gameObject);
+                    //Even more fun would probably be to call a method from the script on destroyable objects in range, something like: 
+                    //coll.gameObject.GetComponent<BombBehaviour>().Explode() could be called instead of Destroy engine method, if != null
+                    //but this solution is quick and easy while BombBehaviour invokes Explode at the Start() so it would need some other script all together  
+                }
+            }
+            
+        }
     }
 }
